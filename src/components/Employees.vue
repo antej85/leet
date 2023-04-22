6b13cadadbcd9c17443eae409362d0957d59c2e7
Andreas <andreas@AJM2.local>
2023-04-22 22:52:13 +0200

    Moved logic to store to prepare for filter function

--- src/components/Employees.vue
+++ src/components/Employees.vue
@@ -26,28 +26,12 @@
   
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Ref } from 'vue'
-import axios from 'axios';
-
+import { useEmployeesStore } from '@/stores/empoyees';
 import EmployeeCard from '@/components/EmployeeCard.vue';
-import type { Employee } from '@/interfaces/employees.d.ts';
-  
-const API_KEY = import.meta.env.VITE_1337_API_KEY;
+import { storeToRefs } from 'pinia';
 
-const fetchError = ref(false);
-const employeeList: Ref<Employee[]> = ref<Employee[]>([]);
-
-// Get employees
-axios.get('https://api.1337co.de/v3/employees', {
-  headers: {
-    'Authorization': API_KEY
-  }
-})
-  .then(res => employeeList.value = res.data)
-  .catch(err => {
-    console.log(err);
-    fetchError.value = true;
-  });
+const employeesStore = useEmployeesStore();
+const { employeeList, fetchError } = storeToRefs(employeesStore);
 </script>
   
 <style lang="scss" scoped>
---
+++ src/stores/empoyees.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Ref } from 'vue'
+import axios from 'axios';
+import type { Employee } from '@/interfaces/employees.d.ts';
+
+export const useEmployeesStore = defineStore('employees', () => {
+  const API_KEY = import.meta.env.VITE_1337_API_KEY;
+  
+  const fetchError = ref(false);
+  const employeeList: Ref<Employee[]> = ref<Employee[]>([]);
+
+  axios.get('https://api.1337co.de/v3/employees', {
+  headers: {
+    'Authorization': API_KEY
+  }
+})
+  .then(res => employeeList.value = res.data)
+  .catch(err => {
+    console.log(err);
+    fetchError.value = true;
+  });
+  
+  return {
+    employeeList,
+    fetchError
+  }
+});
