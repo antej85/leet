9483c2e0e5d47d729696caa20cc340afa645db4d
Andreas <andreas@AJM2.local>
2023-04-22 20:09:26 +0200

    Initial commit, clean up vue3 starter template

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_1337_API_KEY: string
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+console.log(import.meta.env.VITE_1337_API_KEY)
+</script>
+
+<template>
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
