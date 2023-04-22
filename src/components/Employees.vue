48f6774d0198873cc9d3d3699974c10f054e8047
Andreas <andreas@AJM2.local>
2023-04-22 22:55:02 +0200

    remove unused ref import in component after move to pinia

--- src/components/Employees.vue
+++ src/components/Employees.vue
@@ -25,7 +25,6 @@
 </template>
   
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useEmployeesStore } from '@/stores/empoyees';
 import EmployeeCard from '@/components/EmployeeCard.vue';
 import { storeToRefs } from 'pinia';
